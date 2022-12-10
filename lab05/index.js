d633817aec1b58d3d2bdcddcf69396f8e6b57f40
Eicot <zha232zha@gmail.com>
2022-12-10 06:43:09 +0000

    first commit

---
+++ lab05/index.js
@@ -0,0 +1,37 @@
+// 1. SETUP EXPRESS
+const express = require('express');
+const hbs = require('hbs');
+const wax = require('wax-on');
+
+// 1a. create the app
+const app = express();
+
+// 1b setup our view engine (aka template engine)
+// tell Express that we are using hbs
+app.set('view engine', 'hbs');
+
+// 1c setup wax-on
+wax.on(hbs.handlebars);
+
+// 1d tell wax-on where to find the layout files
+wax.setLayoutPath("./views/layouts");
+
+// 1e use static files (i.e, images, css, js etc. -- that is, all content not generated
+// by our routes)
+app.use(express.static('public'));
+
+// 2. CREATE ROUTES
+app.get('/', function(req,res){
+    // res.render() will process the content
+    // from the hbs file (in the first parameter)
+    // and send it back as HTML to the browser
+    const fruits = ["Apples", "Bananas", "Dragonfruit"];
+    res.render("index",{
+        "ourFruits":fruits
+    })
+})
+
+// 3. START SERVER (No routers after you've started server)
+app.listen(3000, function(){
+    console.log("Server has started");
+})
